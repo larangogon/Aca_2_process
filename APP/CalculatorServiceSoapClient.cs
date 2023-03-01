0f94e825d74fadb90cb90df1b597a2baae36ad52
Leidy Arango <leidy.arango@evertecinc.com>
2023-03-01 08:40:50 -0500

    feat: add process

---
+++ APP/CalculatorServiceSoapClient.cs
@@ -0,0 +1,30 @@
+namespace APP
+{
+    CalculatorServiceSoapClient client = new CalculatorServiceSoapClient();
+
+    double value1 = 100.00D;
+    double value2 = 15.99D;
+    double result = client.Add(value1, value2);
+    Console.WriteLine("Add({0},{1}) = {2}", value1, value2, result);
+
+    value1 = 145.00D;
+    value2 = 76.54D;
+    result = client.Subtract(value1, value2);
+    Console.WriteLine("Subtract({0},{1}) = {2}", value1, value2, result);
+
+    value1 = 9.00D;
+    value2 = 81.25D;
+    result = client.Multiply(value1, value2);
+    Console.WriteLine("Multiply({0},{1}) = {2}", value1, value2, result);
+
+    value1 = 22.00D;
+    value2 = 7.00D;
+    result = client.Divide(value1, value2);
+    Console.WriteLine("Divide({0},{1}) = {2}", value1, value2, result);
+
+    client.Close();
+
+    Console.WriteLine();
+    Console.WriteLine("Press <ENTER> to terminate client.");
+    Console.ReadLine();
+}
